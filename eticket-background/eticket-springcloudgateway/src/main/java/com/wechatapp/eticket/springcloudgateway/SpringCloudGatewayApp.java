d0717768230936a3a8469152abbdfb79a2c445b7
virgozx0903 <1746920270@qq.com>
2019-11-10 16:50:50 +0800

    切换节点

--- eticket-background/eticket-springcloudgateway/src/main/java/com/wechatapp/eticket/springcloudgateway/SpringCloudGatewayApp.java
+++ eticket-background/eticket-springcloudgateway/src/main/java/com/wechatapp/eticket/springcloudgateway/SpringCloudGatewayApp.java
@@ -22,11 +22,4 @@ public class SpringCloudGatewayApp
 			log.error(e.getMessage(), e);
 		}
 	}
-	
-	@Bean
-	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-		return builder.routes().route(
-				p -> p.path("/get").filters(f -> f.addRequestHeader("Hello", "World")).uri("http://httpbin.org:80"))
-				.build();
-	}
 }
